5c943625c85d7c83deff30cd094133cdd5c98577
MrZaxter <SymphonyOfFinality@gmail.com>
2025-02-22 16:26:23 +0300

    Added loading status, changed the favicon and title

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 // created by https://github.com/Discordianis/moysklad
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
